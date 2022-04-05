a92fba9693bb8c244c962762bd52852199eb7b4e
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-04-05 21:17:43 +0600

    Added three components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import Nav from "./components/Nav/Nav";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-     
+     <Nav />
+     <Header/>
+     <Footer/>
     </div>
   );
 }
 
 export default App;
+
+
+// https://pm1.narvii.com/6834/ca19888380f246d78f14871eb1aeea75e07aafddv2_hq.jpg
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,21 @@
+function Nav() {
+  return ( 
+    <nav className="Nav">
+    <div>All the most fashionable</div>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Categories</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Products</a></li>
+      <li><a href="#">Contacts</a></li>
+      <li><a href="/">Home</a></li>
+      <li><a href="/">Categories</a></li>
+      <li><a href="/">About</a></li>
+      <li><a href="/">Products</a></li>
+      <li><a href="/">Contacts</a></li>
+    </ul>
+  </nav>
+   );
+}
+
+export default Nav;
