19093087b0f821787926640fdfd016fe64c2eda0
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-04-18 16:00:15 +0600

    Renew header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,24 +2,22 @@ import "./Header.css";
 import headerImage from "../../assets/models4.jpg";
 
 
-function Header() {
+function Header({ image, title, children }) {
   const style = {
     backgroundImage: `url(${headerImage})`
   };
 
-function Header({ image, title, children }) {
   return (
-    <header className="Header" style={style}>
-      <h1>The most fresh coffee beans</h1>
-      <p>This is an internet shop</p>
- 
-    <header className="Header">
-      <img src={image} />
-      <section>
-        <h1>{title}</h1>
-        <p>{children}</p>
-      </section>
-    </header>
-  );
+    <>
+      <header className="Header">
+        <img src={image} />
+        <section>
+          <h1>{title}</h1>
+          <p>{children}</p>
+        </section>
+      </header>
+    </>
+  )
 }
+
 export default Header;
