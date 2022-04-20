0db0866c2a15a5290413a5321abaada804091fdb
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-04-20 15:04:20 +0600

    Delete article

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,15 +6,7 @@ import headerImage from "../assets/home.jpg";
 function Home() {
   return (
     <>
-      <article>
-        <h1>This is home page.</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex pariatur
-          voluptatum doloribus enim repudiandae ullam sequi quo suscipit
-          voluptas id, laboriosam necessitatibus, dolore corrupti. Explicabo
-          iure doloribus illum repudiandae tenetur.
-        </p>
-      </article>
+  
 
       <Header
         title="Welcome to our fashion clothing store!"
