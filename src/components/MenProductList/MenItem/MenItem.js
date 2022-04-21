51578d812e7254844fd010a3d62bba6431537fba
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-04-21 22:17:21 +0600

    Added menImage to MenItem

--- src/components/MenProductList/MenItem/MenItem.js
+++ src/components/MenProductList/MenItem/MenItem.js
@@ -1,8 +1,17 @@
-function MenItem() {
+import { Link } from "react-router-dom";
+import classes from "./MenItem.module.css";
+import menImage from "../../../assets/men-pro1.jpg";
+function MenItem({product}) {
   return (  
-    <div className="MenItem"></div>
+    <div className="MenItem">
+      <img src={menImage}/>
+      <div>
+        <Link to="/">Cotton drawstring hoodie</Link>
+        <div className={classes.price}>$290</div>
+      </div>
+    </div>
   );
 
-<div className=""></div>}
+  }
 
 export default MenItem;
