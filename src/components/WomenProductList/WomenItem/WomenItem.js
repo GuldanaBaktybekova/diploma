8156408cfe7257601d37b870100fd605d56704f4
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-04-21 23:12:08 +0600

    Added WomenItem component

---
+++ src/components/WomenProductList/WomenItem/WomenItem.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import classes from "./WomenItem.module.css";
+import womenImage from "../../../assets/women-pro1.jpg";
+function MenItem({product}) {
+  return (  
+    <div className="WomenItem">
+      <img src={womenImage} alt="women"/>
+      <div>
+        <Link to="/">Cotton drawstring hoodie</Link>
+        <div className={classes.price}>$290</div>
+      </div>
+      <img src={womenImage} alt="women"/>
+      <div>
+        <Link to="/">Cotton drawstring hoodie</Link>
+        <div className={classes.price}>$290</div>
+      </div>
+      <img src={womenImage} alt="women"/>
+      <div>
+        <Link to="/">Cotton drawstring hoodie</Link>
+        <div className={classes.price}>$290</div>
+      </div>
+    </div>
+  );
+
+  }
+
+export default MenItem;
